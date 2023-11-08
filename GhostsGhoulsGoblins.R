12a5c5d82a65cedf06ee6c817013cfca37680ac8
Jared Lane <jared@gimlisys.com>
2023-11-08 15:02:30 -0700

    Boosting 11-8-23

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -253,7 +253,7 @@ baked_missing <- bake(prep, new_data = GGG_missing)
 library(bonsai)
 library(lightgbm)
 
-boost_model <- boost_tree(tree_depth = tune(),
+boost_model <- boost_tree(tree_depth = 250,
                           trees = tune(), 
                           learn_rate = tune()) %>%
   set_engine("lightgbm") %>%
@@ -263,8 +263,7 @@ boost_wf <- workflow() %>%
   add_recipe(my_recipe) %>%
   add_model(boost_model)
 
-tuning_grid <- grid_regular(tree_depth(),
-                            trees(),
+tuning_grid <- grid_regular(trees(),
                             learn_rate(),
                             levels = 5)
 
@@ -293,6 +292,200 @@ submission <- submission %>% rename("id" = "id", "type" = "...2")
 vroom_write(submission, "./GGG/submission.csv", col_names = TRUE, delim = ", ")
 
 ####BARTBBARTBART
-bart_model <- bart(trees = tune()) %>%
+bart_model <- parsnip::bart(trees = tune()) %>%
   set_engine("dbarts") %>%
   set_mode("classification")
+
+bart_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(bart_model)
+
+tuning_grid <- grid_regular(trees(),
+                            levels = 5)
+
+folds <- vfold_cv(GGG_tr, v = 5, repeats = 1)
+
+CV_results <- bart_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(accuracy))
+
+bestTune <- CV_results %>%
+  select_best("accuracy")
+
+final_wf <- bart_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = GGG_tr)
+
+GGG_predictions <- final_wf %>% predict(new_data = GGG_te, type = "class")
+
+
+######
+#formats submissions properly
+submission <- bind_cols(GGG_te %>% select(id), GGG_predictions$.pred_class)
+submission <- submission %>% rename("id" = "id", "type" = "...2")
+#writes onto a csv
+vroom_write(submission, "./GGG/submission.csv", col_names = TRUE, delim = ", ")
+
+
+###SVM Scribbling
+library(themis)
+library(vroom)
+
+library(tensorflow)
+library(embed)
+library(tidyverse)
+library(tidymodels)
+my_recipe <- recipe(type ~ ., data = GGG_tr) %>%
+  step_normalize(all_numeric_predictors())
+prep <- prep(my_recipe)
+baked_tr <- bake(prep, new_data = GGG_tr)
+baked_missing <- bake(prep, new_data = GGG_missing)
+
+svmRadial <- svm_rbf(rbf_sigma = .00318, cost = 2.41) %>%
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svm_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(svmRadial)
+
+
+
+tuning_grid <- grid_regular(rbf_sigma(),
+                            cost(),
+                            levels = 5)
+
+final_wf <- svm_wf %>%
+  fit(data = GGG_tr)
+
+GGG_predictions <- final_wf %>% predict(new_data = GGG_te, type = "class")
+
+
+######
+#formats submissions properly
+submission <- bind_cols(GGG_te %>% select(id), GGG_predictions$.pred_class)
+submission <- submission %>% rename("id" = "id", "type" = "...2")
+#writes onto a csv
+vroom_write(submission, "./GGG/submission.csv", col_names = TRUE, delim = ", ")
+
+
+##NAIVE BAYES
+library(tidymodels)
+library(vroom)
+library(discrim)
+library(themis)
+
+
+
+library(embed)
+library(tidyverse)
+library(tidymodels)
+
+GGG_tr <- vroom("./GGG/train.csv")
+GGG_te <- vroom("./GGG/test.csv")
+GGG_missing <- vroom("./GGG/trainWithMissingValues.csv")
+
+##MEAN Implementation
+my_recipe <- recipe(type ~ ., data = GGG_tr) %>%
+  step_impute_bag(all_numeric_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+prep <- prep(my_recipe)
+baked_tr <- bake(prep, new_data = GGG_tr)
+baked_missing <- bake(prep, new_data = GGG_missing)
+
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(nb_model)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 10)
+
+folds <- vfold_cv(GGG_tr, v = 6, repeats = 1)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(accuracy))
+
+bestTune <- CV_results %>%
+  select_best("accuracy")
+
+final_wf <- nb_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = GGG_tr)
+
+GGG_predictions <- final_wf %>% predict(new_data = GGG_te, type = "class")
+
+
+######
+#formats submissions properly
+submission <- bind_cols(GGG_te %>% select(id), GGG_predictions$.pred_class)
+submission <- submission %>% rename("id" = "id", "type" = "...2")
+#writes onto a csv
+vroom_write(submission, "./GGG/submission.csv", col_names = TRUE, delim = ", ")
+
+
+###MODEL STACKING
+library(stacks)
+
+untunedModel <- control_stack_grid()
+tunedModel <- control_stack_resamples()
+
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(nb_model)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 10)
+
+folds <- vfold_cv(GGG_tr, v = 6, repeats = 1)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc),
+            control = untunedModel)
+
+svmRadial <- svm_rbf(rbf_sigma = tune(), cost = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svm_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(svmRadial)
+
+tuning_grid_2 <- grid_regular(rbf_sigma(),
+                            cost(),
+                            levels = 5)
+
+CV_results_2 <- svm_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid_2,
+            metrics = metric_set(roc_auc),
+            control = untunedModel)
+
+my_stack <- stacks() %>%
+  add_candidates(CV_results) %>%
+  add_candidates(CV_results_2)
+
+stack_mod <- my_stack %>%
+  blend_predictions() %>%
+  fit_members()
+
+stackData <- as_tibble(my_stack)
+
+GGG_predictions <- predict(stack_mod,
+                                new_data = GGG_te, type = "class")
+
+
